ccf3d45821412ffe712757656c3ed4b9928966b3
Ferdinand Malcher <ferdinand@malcher.media>
2021-04-21 12:14:38 +0200

    Interface Book

---
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  books: Book[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.books = [
+      {
+        isbn: '123',
+        title: 'Angular',
+        description: 'Grundlagen und mehr',
+        price: 36.90,
+        rating: 5
+      },
+      {
+        isbn: '456',
+        title: 'React',
+        description: 'Das andere Framework',
+        price: 32.90,
+        rating: 3
+      }
+    ];
+  }
+
+}
