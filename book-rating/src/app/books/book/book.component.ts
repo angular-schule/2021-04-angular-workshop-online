e900a970010e37d0299940d6c31dd1bbc0ddd4fc
Ferdinand Malcher <ferdinand@malcher.media>
2021-04-22 11:21:28 +0200

    Events

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
@@ -9,6 +9,8 @@ import { Book } from '../shared/book';
 export class BookComponent implements OnInit {
 
   @Input() book: Book;
+  @Output() rateUp = new EventEmitter<Book>();
+  @Output() rateDown = new EventEmitter<Book>();
 
   constructor() { }
 
@@ -17,6 +19,14 @@ export class BookComponent implements OnInit {
 
   getStarsArray(): any[] {
     return new Array(this.book.rating);
+  }
+
+  doRateUp(): void {
+    this.rateUp.emit(this.book);
+  }
+
+  doRateDown(): void {
+    this.rateDown.emit(this.book);
   }
 
 }
--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -31,4 +31,12 @@ export class DashboardComponent implements OnInit {
     ];
   }
 
+  doRateUp(book: Book) {
+    console.log('UP', book);
+  }
+  
+  doRateDown(book: Book) {
+    console.log('DOWN', book);
+  }
+
 }
