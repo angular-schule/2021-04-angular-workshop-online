0a48d3557deec3e1d9e2fb63913392244c3514f1
Ferdinand Malcher <ferdinand@malcher.media>
2021-04-28 13:56:46 +0200

    Typeahead-Suche

--- book-rating/src/app/books/search/search.component.ts
+++ book-rating/src/app/books/search/search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, filter, switchMap } from 'rxjs/operators';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-search',
@@ -9,13 +13,24 @@ import { FormControl } from '@angular/forms';
 export class SearchComponent implements OnInit {
 
   searchControl: FormControl;
+  results$: Observable<Book[]>;
 
-  constructor() { }
+  constructor(private bs: BookStoreService) { }
 
   ngOnInit(): void {
     this.searchControl = new FormControl('');
 
-    this.searchControl.valueChanges.subscribe(e => console.log(e));
+    this.results$ = this.searchControl.valueChanges.pipe(
+      filter((e: string) => e.length >= 3),
+      debounceTime(1000),
+      switchMap(term => this.bs.search(term))
+    );
   }
+
+  /*
+  - Bug, wenn leerer Suchbegriff
+  - Ladeanzeige
+  - Anzeige, dass keine Ergebnisse vorhanden
+  */
 
 }
