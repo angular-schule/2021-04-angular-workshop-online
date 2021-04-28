cbfb8faf9ef758444db045ed2b69da6cf1ab2bef
Ferdinand Malcher <ferdinand@malcher.media>
2021-04-28 15:34:25 +0200

    NgRx

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
 import { BookStoreService } from '../shared/book-store.service';
+import { loadBooks } from '../store/book.actions';
+import { selectBooks } from '../store/book.selectors';
 
 @Component({
   selector: 'br-dashboard',
@@ -12,11 +15,16 @@ export class DashboardComponent implements OnInit {
 
   books: Book[];
   
-  constructor(private rs: BookRatingService, private bs: BookStoreService) { }
+  constructor(private rs: BookRatingService, private store: Store) { }
 
   ngOnInit(): void {
-    this.bs.getAll()
-      .subscribe(books => this.books = books);
+    /*this.bs.getAll()
+      .subscribe(books => this.books = books);*/
+    this.store.dispatch(loadBooks());
+
+    // this.store.pipe(map(state => selectBooks(state)))
+    const books$ = this.store.pipe(select(selectBooks));
+    books$.subscribe(books => this.books = books);
   }
 
   doRateUp(book: Book) {
